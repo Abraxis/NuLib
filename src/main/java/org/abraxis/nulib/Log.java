b1fbfaca6eaae1e514337b81634e0e0e78ce13be
unknown <RIDB10060@SKHON00057.ridb.corp>
2013-01-05 20:58:03 +0100

    First commit

---
+++ src/main/java/org/abraxis/nulib/Log.java
@@ -0,0 +1,15 @@
+package org.abraxis.nulib;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Log {
+
+    public static Logger getLogger(Class c)
+    {
+        Logger logger = LoggerFactory.getLogger(c);
+
+        return logger;
+    }
+
+}
---
+++ src/test/java/org/abraxis/nulib/LogTest.java
@@ -0,0 +1,24 @@
+package org.abraxis.nulib;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.util.StatusPrinter;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+public class LogTest {
+    @Test
+    public void testGetLogger() throws Exception {
+        Logger logger = Log.getLogger(this.getClass());
+
+        logger.error("Testing error message, date is: {}", new Date().toString());
+        logger.warn("Testing exception", new Exception("Testing exception"));
+        logger.info("Testing info message");
+        logger.debug("Testing debug message");
+
+        LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+        StatusPrinter.print(lc);
+    }
+}
